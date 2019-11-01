26cd552f13f5fcf695cddb0aa5ff947522435137
kou <>
2019-11-01 13:08:19 +0900

    エラーメッセージ表示のif文を削除、user_idの取得方法の変更

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -5,24 +5,17 @@
 <div class="main-wrap">
   <div class="container">
     {!! Form::open(['route' => 'report.store']) !!}
-      {!! Form::input('hidden', 'user_id', Auth::id(), ['class' => 'form-control']) !!}
       <div class="form-group form-size-small {{ $errors->has('reporting_time') ? 'has-error' : '' }}">
         {!! Form::input('date', 'reporting_time', Carbon::now()->format('Y-m-d'), ['class' => 'form-control']) !!}
-        @if($errors->has('reporting_time'))
-          <span class="help-block">{{ $errors->first('reporting_time') }}</span>
-        @endif
+         <span class="help-block">{{ $errors->first('reporting_time') }}</span>
       </div>
       <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
         {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
-        @if($errors->has('title'))
-          <span class="help-block">{{ $errors->first('title') }}</span>
-        @endif
+         <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
       <div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
         {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Content', 'cols' => '50', 'rows' => '10']) !!}
-        @if($errors->has('content'))
-          <span class="help-block">{{ $errors->first('content') }}</span>
-        @endif
+         <span class="help-block">{{ $errors->first('content') }}</span>
       </div>
       {!! Form::submit('Add', ['class' => 'btn btn-success pull-right']) !!}
     {!! Form::close() !!}
--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -1,32 +1,25 @@
 @extends ('common.user')
 @section ('content')
-	
-	<h2 class="brand-header">日報編集</h2>
-  <div class="main-wrap">
-		<div class="container">
-			{!! Form::open(['route' => ['report.update', $report->user_id], 'method' => 'PUT']) !!}
-        {!! Form::input('hidden', 'user_id', Auth::id(), ['class' => 'form-control']) !!}
-        <div class="form-group form-size-small {{ $errors->has('reporting_time') ?  'has-error' : '' }}">
-          {!! Form::input('date', 'reporting_time', $report->reporting_time->format('Y-m-d'), ['class' => 'form-control']) !!}
-          @if($errors->has('reporting_time'))
-            <span class="help-block">{{ $errors->first('reporting_time') }}</span>
-          @endif
-        </div>
-        <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
-          {!! Form::input('text', 'title', $report->title, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
-          @if($errors->has('title'))
-            <span class="help-block">{{ $errors->first('title') }}</span>
-          @endif
-        </div>
-        <div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
-          {!! Form::textarea('content', $report->content, ['class' => 'form-control', 'placeholder' => 'Content', 'cols' => '50', 'rows' => '10']) !!}
-          @if($errors->has('content'))
-            <span class="help-block">{{ $errors->first('content') }}</span>
-          @endif
-        </div>
-        {!! Form::submit('UPDATE', ['class' => 'btn btn-success pull-right']) !!}
-			{!! Form::close() !!}
-		</div>
-	</div>
+
+<h2 class="brand-header">日報編集</h2>
+<div class="main-wrap">
+  <div class="container">
+    {!! Form::open(['route' => ['report.update', $report->user_id], 'method' => 'PUT']) !!}
+      <div class="form-group form-size-small {{ $errors->has('reporting_time') ?  'has-error' : '' }}">
+        {!! Form::input('date', 'reporting_time', $report->reporting_time->format('Y-m-d'), ['class' => 'form-control']) !!}
+         <span class="help-block">{{ $errors->first('reporting_time') }}</span>
+      </div>
+      <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
+        {!! Form::input('text', 'title', $report->title, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
+        <span class="help-block">{{ $errors->first('title') }}</span>
+      </div>
+      <div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
+        {!! Form::textarea('content', $report->content, ['class' => 'form-control', 'placeholder' => 'Content', 'cols' => '50', 'rows' => '10']) !!}
+        <span class="help-block">{{ $errors->first('content') }}</span>
+      </div>
+      {!! Form::submit('UPDATE', ['class' => 'btn btn-success pull-right']) !!}
+    {!! Form::close() !!}
+  </div>
+</div>
 
 @endsection
