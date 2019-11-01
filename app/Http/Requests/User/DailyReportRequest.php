801c3c6a3f582d828c9a13ff85b46d0f4b1ee26a
kou <>
2019-11-01 13:07:02 +0900

    エラーメッセージrequiredの修正

--- app/Http/Requests/User/DailyReportRequest.php
+++ app/Http/Requests/User/DailyReportRequest.php
@@ -24,20 +24,18 @@ class DailyReportRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required|max:30',
-            'content' => 'required|max:1000',
+            'title'          => 'required|max:30',
+            'content'        => 'required|max:1000',
             'reporting_time' => 'required|before:now',
         ];
     }
     
     public function messages() {
         return [
-            'title.required' => '入力必須の項目です',
-            'title.max' => '30文字以内で入力してください',
-            'content.required'  => '入力必須の項目です',
-            'content.max' => '1000文字以内で入力してください',
-            'reporting_time.required'  => '入力必須の項目です',
-            'reporting_time.before' => '今日以前の日付を入力してください。',
+            'required'                => '入力必須の項目です',
+            'title.max'               => '30文字以内で入力してください',
+            'content.max'             => '1000文字以内で入力してください',
+            'reporting_time.before'   => '今日以前の日付を入力してください。',
         ];
     }
 }
