13d64a8f5406a9e4268150cfbba7f67299d16758
kou <>
2019-11-07 15:50:21 +0900

    seed実行時、truncateでテーブルを空にする際に、外部キー制約により削除できなかったため、外部キーチェックを行わない仕様へ変更

--- database/seeds/TagCategoriesSeeder.php
+++ database/seeds/TagCategoriesSeeder.php
@@ -6,7 +6,9 @@ class TagCategoriesSeeder extends Seeder
 {
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         DB::table('tag_categories')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         DB::table('tag_categories')->insert([
             [
                 'name' => 'front',
