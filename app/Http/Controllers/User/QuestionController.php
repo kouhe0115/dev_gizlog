ddf7d28dcfc8ddab875177fe4231bb11b18185de
kou <>
2019-11-08 10:34:31 +0900

    一覧表示、マイページ表示処理の実装

---
+++ app/Http/Controllers/User/QuestionController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Question;
+use App\Models\TagCategory;
+use Illuminate\Support\Facades\Auth;
+
+class QuestionController extends Controller
+{
+    private $question;
+    private $category;
+    
+    public function __construct(Question $question, TagCategory $category)
+    {
+        $this->middleware('auth');
+        $this->question = $question;
+        $this->category = $category;
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $inputs = $request->all();
+        $questions = $this->question->getQuestion($inputs);
+        $categories = $this->category->get();
+        
+        return view('user.question.index',
+               compact('questions', 'categories', 'inputs'));
+    }
+    
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    
+    public function myPage($id)
+    {
+        $inputs['user_id'] = Auth::id();
+        $questions = $this->question->getQuestion($inputs);
+        
+        return view('user.question.mypage', compact('questions'));
+    }
+}
