01de1470cbea3f58043dfd1e12ef591d2faf3fbf
kou <>
2019-11-18 00:41:15 +0900

    addendance テーブルの作成

---
+++ database/migrations/2019_11_15_084704_create_attendances_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index();
+            $table->dateTime('start_time')->nullable();
+            $table->dateTime('end_time')->nullable();
+            $table->string('request_content', 500)->nullable();
+            $table->integer('absent_flg')->default(0);
+            $table->string('absent_reason', 500)->nullable();
+            $table->dateTime('date');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
