776e1ea72baf1fd888bb421419d362daa9706de0
kou <>
2019-11-18 00:43:05 +0900

    コントローラの作成 出勤、退勤機能実装

---
+++ app/Http/Controllers/User/AttendanceController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Attendance;
+use Illuminate\Http\Request;
+use Auth;
+use Carbon\Carbon;
+
+class AttendanceController extends Controller
+{
+    private $attendance;
+    
+    /**
+     * コンストラクター
+     *
+     * Attendance $attendance
+     * @param Attendance $attendance
+     */
+    public function __construct(Attendance $attendance)
+    {
+        $this->attendance = $attendance;
+    }
+    
+    /**
+     * 勤怠管理画面の表示
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $userId = Auth::id();
+        $d = Carbon::now()->format('Y-m-d');
+        $attendance = $this->attendance->where('date', $d)->where('user_id', $userId)->first();
+        return view('user.attendance.index', compact('attendance'));
+    }
+    
+    /**
+     * 出勤時間の登録
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function setStartAbsent(Request $request)
+    {
+        $inputs = $request->all();
+        $inputs['user_id'] = Auth::id();
+        $inputs['date'] = Carbon::now()->format('Y-m-d');
+        $this->attendance->fill($inputs)->save();
+        return redirect()->route('attendance');
+    }
+    
+    /**
+     * 退勤時間の登録
+     *
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function setEndAbsent(Request $request, $id)
+    {
+        $inputs = $request->all();
+        $this->attendance->find($id)->fill($inputs)->save();
+        return redirect()->route('attendance');
+    }
+    
+    public function mypage()
+    {
+        return view('user.attendance.mypage');
+    }
+}
