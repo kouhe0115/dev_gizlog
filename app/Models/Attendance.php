e19f9b86efb5463b1794999f52b03daae26b139e
kou <>
2019-11-18 00:42:19 +0900

    モデルの作成

---
+++ app/Models/Attendance.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'start_time',
+        'end_time',
+        'request_content',
+        'absent_flg',
+        'absent_reason',
+        'date',
+    ];
+    
+    protected $dates = [
+        'start_time',
+        'end_time',
+        'date',
+        'deleted_at',
+    ];
+    
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+    
+    
+}
+
+
+
+
