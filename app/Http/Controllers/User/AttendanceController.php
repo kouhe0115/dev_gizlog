c00331fb9703e9a6ad90a70d00902a56f8f7f07f
kou <>
2019-11-18 23:43:05 +0900

    不要な use の削除

--- app/Http/Controllers/User/AttendanceController.php
+++ app/Http/Controllers/User/AttendanceController.php
@@ -3,29 +3,23 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use App\Models\Attendance;
-use Illuminate\Http\Request;
-use Auth;
 use App\Service\AttendanceService;
 use App\Http\Requests\User\AttendanceRequest;
 
 class AttendanceController extends Controller
 {
-    private $attendance;
     private $attendanceService;
-    
+
     /**
      * AttendanceController constructor.
-     * @param Attendance $attendance
      * @param AttendanceService $attendanceService
      */
-    public function __construct(Attendance $attendance, AttendanceService $attendanceService)
+    public function __construct(AttendanceService $attendanceService)
     {
         $this->middleware('auth');
-        $this->attendance = $attendance;
         $this->attendanceService = $attendanceService;
     }
-    
+
     /**
      * 勤怠管理画面の表示
      *
@@ -33,40 +27,37 @@ class AttendanceController extends Controller
      */
     public function index()
     {
-        $userId = Auth::id();
-        $attendance = $this->attendanceService->getByTodayAttendance($userId);
+        $attendance = $this->attendanceService->getByTodayAttendance();
         return view('user.attendance.index', compact('attendance'));
     }
     
     /**
      * 出勤時間の登録
      *
-     * @param Request $request
+     * @param AttendanceRequest $request
      * @return \Illuminate\Http\RedirectResponse
      */
     public function setStartAbsent(AttendanceRequest $request)
     {
         $inputs = $request->validated();
-        $inputs['user_id'] = Auth::id();
-        $inputs['date'] = $this->attendanceService->getNowDate();
-        $this->attendance->fill($inputs)->save();
+        $this->attendanceService->setStartTime($inputs);
         return redirect()->route('attendance');
     }
     
     /**
      * 退勤時間の登録
      *
-     * @param Request $request
+     * @param AttendanceRequest $request
      * @param $id
      * @return \Illuminate\Http\RedirectResponse
      */
     public function setEndAbsent(AttendanceRequest $request, $id)
     {
         $inputs = $request->validated();
-        $this->attendance->find($id)->fill($inputs)->save();
+        $this->attendanceService->setEndTime($inputs, $id);
         return redirect()->route('attendance');
     }
-    
+
     /**
      * 欠席理由画面の表示
      *
@@ -80,21 +71,18 @@ class AttendanceController extends Controller
     /**
      * 欠席、理由の登録
      *
-     * @param Request $request
+     * @param AttendanceRequest $request
      * @return \Illuminate\Http\RedirectResponse
      */
     public function setAbsence(AttendanceRequest $request)
     {
         $inputs = $request->validated();
-        $inputs['user_id'] = Auth::id();
-        $inputs['absent_flg'] = 1;
-        $inputs['date'] = $this->attendanceService->getNowDate();
-        $this->attendance->fill($inputs)->save();
+        $this->attendanceService->setAbsence($inputs);
         return redirect()->route('attendance');
     }
-    
+
     /**
-     * 修正申請画面表示
+     * 勤怠修正申請画面表示
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
@@ -104,20 +92,18 @@ class AttendanceController extends Controller
     }
     
     /**
-     * 修正申請を登録する
+     * 修正申請の登録
      *
-     * @param Request $request
+     * @param AttendanceRequest $request
      * @return \Illuminate\Http\RedirectResponse
      */
     public function createModify(AttendanceRequest $request)
     {
         $inputs = $request->validated();
-        $inputs['user_id'] = Auth::id();
-        $attendance = $this->attendanceService->getAttendanceBySearchDate($inputs);
-        $attendance->fill($inputs)->save();
+        $this->attendanceService->setRequest($inputs);
         return redirect()->route('attendance');
     }
-    
+
     /**
      * ログイン中のユーザーの勤怠記録の表示
      *
@@ -129,6 +115,7 @@ class AttendanceController extends Controller
         $attendances = $this->attendanceService->getByUserId($userId);
         $attendancesCount = $this->attendanceService->getAttendancesCount($userId);
         $totalLearningTime = $this->attendanceService->getTotalLearningTime($userId);
-        return view('user.attendance.mypage', compact('attendances', 'totalLearningTime', 'attendancesCount'));
+        return view('user.attendance.mypage',
+            compact('attendances', 'totalLearningTime', 'attendancesCount'));
     }
 }
