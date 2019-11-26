896621769e62eeccc6a4c33af943540cd957048f
kou <>
2019-11-26 18:59:45 +0900

    middlewareを route 定義

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
  */
 
-Route::group(['prefix' => '/', 'user.', 'namespace' => 'User'], function () {
+Route::group(['prefix' => '/', 'user.', 'namespace' => 'User', 'middleware' => 'auth'], function () {
     Auth::routes();
 
     Route::get('/', function () {
