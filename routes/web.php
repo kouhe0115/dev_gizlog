b6b2dde85c302318ae91a025ad922ac97b69ea36
kou <>
2019-11-27 14:41:27 +0900

    routeのmiddlewareの修正（auth をroute に書いて適応させる方法は理解したが、他の重複してリダイレクトが複数回実行され動かないため）

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
  */
 
-Route::group(['prefix' => '/', 'user.', 'namespace' => 'User', 'middleware' => 'auth'], function () {
+Route::group(['prefix' => '/', 'user.', 'namespace' => 'User'], function () {
     Auth::routes();
 
     Route::get('/', function () {
