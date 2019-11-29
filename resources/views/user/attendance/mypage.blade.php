378135bfc118d44ac3133abffa74a9b5c346757d
kou <>
2019-11-29 11:35:35 +0900

    formatの修正(user)

--- resources/views/user/attendance/mypage.blade.php
+++ resources/views/user/attendance/mypage.blade.php
@@ -35,8 +35,8 @@
         @foreach ($attendances as $attendance)
       <tr class="row {{ $attendance->is_absent ? 'absent-row' : '' }}">
           <td class="col-xs-2">{{ $attendance->date->format('Y-m-d') ?? '-' }}</td>
-          <td class="col-xs-3">{{ $attendance->start_time ?? '-'}}  </td>
-          <td class="col-xs-3">{{ $attendance->end_time ?? '-' }}</td>
+          <td class="col-xs-3">{{ $attendance->start_time ? $attendance->start_time->format('H:i') : '-'}}  </td>
+          <td class="col-xs-3">{{ $attendance->end_time ? $attendance->end_time->format('H:i') : '-' }}</td>
           <td class="col-xs-2">@if($attendance->is_absent) 欠席 @elseif(is_null($attendance->end_time)) 研修中 @elseif(!$attendance->is_absent) 出社 @endif</td>
           <td class="col-xs-2">{{ $attendance->request_content ? '申請中' : '-' }}</td>
       </tr>
