2a387570396485314e296bfbcb14857b7c43c938
kou <>
2019-12-03 23:57:46 +0900

    エラーメッセージの表示

--- resources/views/user/question/create.blade.php
+++ resources/views/user/question/create.blade.php
@@ -5,7 +5,7 @@
 <div class="main-wrap">
   <div class="container">
     {!! Form::open(['route' => 'question.confirm', 'method' => 'post']) !!}
-      <div class="form-group {{ $errors->has('tag_category_id')? 'has-error' : '' }}">
+      <div class="form-group {{ $errors->has('tag_category_id') ? 'has-error' : '' }}">
         <select name='tag_category_id' class = "form-control selectpicker form-size-small" id ="pref_id">
           <option value="">Select category</option>
           @foreach($categories as $category)
@@ -14,11 +14,11 @@
         </select>
         <span class="help-block">{{ $errors->first('tag_category_id') }}</span>
       </div>
-      <div class="form-group {{ $errors->has('title')? 'has-error' : '' }}">
+      <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
         {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'title']) !!}
         <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
-      <div class="form-group {{ $errors->has('content')? 'has-error' : '' }}">
+      <div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
         {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Please write down your question here...']) !!}
         <span class="help-block">{{ $errors->first('content') }}</span>
       </div>
